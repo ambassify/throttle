1c85f26f0be66d7f4487afdb0788a9f65d0cac49
Jorgen Evens <jorgen@evens.eu>
2020-04-15 11:45:03 +0200

    [weakref] Add support for tc39 WeakRef

---
+++ test/weakref.js
@@ -0,0 +1,69 @@
+const assert = require('assert');
+
+describe('weakref', () => {
+
+    let weak;
+
+    before(() => {
+        weak = require('../weakref');
+    });
+
+    it ('should have WeakRef when harmony flag is enabled', function() {
+        const { HARMONY_OPTIONS = '' } = process.env;
+
+        if (!/--harmony-weak-refs/.test(HARMONY_OPTIONS))
+            return this.skip();
+
+        assert.notEqual(typeof WeakRef, 'undefined');
+        assert.notEqual(weak.isUnsupported, true, 'reported unsupported');
+    });
+
+    it ('should create a weak ref', () => {
+        const a = { test: 'yes' };
+
+        const ref = weak(a);
+
+        assert(ref);
+
+        assert.strictEqual(weak.get(ref), a);
+    });
+
+    it ('should create a weak ref', () => {
+        const a = { test: 'yes' };
+
+        const ref = weak(a);
+
+        assert(ref);
+
+        assert.strictEqual(weak.get(ref), a);
+    });
+
+    it ('should detect refs that are not dead yet', () => {
+        const a = { test: 'yes' };
+        const ref = weak(a);
+
+        assert(!weak.isDead(ref), 'weakref is dead');
+    });
+
+    it ('should detect when refs become dead', function(done) {
+        if (weak.isUnsupported)
+            return this.skip();
+
+        this.timeout(15000);
+
+        const ref = (function() {
+            const a = { test: 'yes' };
+            return weak(a);
+        }());
+
+        function isDeadYet() {
+            if (weak.isDead(ref))
+                return done();
+
+            setTimeout(isDeadYet, 250);
+        }
+
+        isDeadYet();
+    });
+
+});
